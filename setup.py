a9f905781ffc974f54d5025f2b820ec83df420dd
StealHD <1099631220@qq.com>
2023-12-14 15:30:41 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flasktest',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='StealHD',
+    author_email='',
+    description=''
+)
